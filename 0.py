75187cf30a7a912b7455cae5d5910afc082b6969
Riki Chandra <rikichandra37@gmail.com>
2022-10-13 14:28:53 +0700

    update

--- 0.py
+++ 0.py
@@ -7,5 +7,7 @@ if jenis_tiket == "ekonomi":
         harga = 20000
     elif waktu_tiket == "malam":
         harga = 50000
-    else waktu_tiket == 0 :
+    else waktu_tiket == 0:
         harga = 0
+
+# adit pacarnya nabila
