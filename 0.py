72562df28ef68fc35a1d20844f12caa600f4fa8a
aditya124512 <adityaalassad@github.com>
2022-10-13 14:10:15 +0700

    commit pertama

---
+++ 0.py
@@ -0,0 +1,10 @@
+jenis_tiket = str(input("MASUKAN JENIS TIKET = "))
+waktu_tiket = str(input("MASUKAN WAKTU TIKET = "))
+jumlah_tiket = int(input("MASUKAN JUMLAH TIKET = "))
+Print("=" * 40)
+if jenis_tiket == "ekonomi":
+    if waktu_tiket == "pagi":
+        harga = 20000
+    elif waktu_tiket == "malam":
+        harga = 50000
+        
